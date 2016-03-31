771241311eb78b25b2b15b9d3ac35e9ebbbfe971
Ivan Magda <imagda15@gmail.com>
2016-03-31 09:05:52 +0900

    Initial commit.
    - Add .gitignore file.
    - Add initial iOS project.

---
+++ iOS/MatrixMath/MatrixMath/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  MatrixMath
+//
+//  Created by Ivan Magda on 31.03.16.
+//  Copyright © 2016 Ivan Magda. All rights reserved.
+//
+
+import UIKit
+
+//---------------------------------------------------------
+// MARK: - AppDelegate: UIResponder, UIApplicationDelegate
+//---------------------------------------------------------
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    //-----------------------------------------------------
+    // MARK: Properties
+    //-----------------------------------------------------
+
+    var window: UIWindow?
+    
+    //-----------------------------------------------------
+    // MARK: UIApplicationDelegate
+    //-----------------------------------------------------
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
---
+++ iOS/MatrixMath/MatrixMath/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MatrixMath
+//
+//  Created by Ivan Magda on 31.03.16.
+//  Copyright © 2016 Ivan Magda. All rights reserved.
+//
+
+import UIKit
+
+//------------------------------------------------
+// MARK: - ViewController: UIViewController
+//------------------------------------------------
+
+class ViewController: UIViewController {
+    
+    //------------------------------------------------
+    // MARK: View Life Cycle
+    //------------------------------------------------
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
