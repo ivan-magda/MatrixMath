5624d62c5e660dc692e23e799610a51f414b2185
Ivan Magda <imagda15@gmail.com>
2016-04-15 00:38:42 +0900

    [POD] Add SVProgressHUD

--- iOS/MatrixMath/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ iOS/MatrixMath/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -85,7 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/ActionSheetPicker_3_0.framework"
+  install_framework "Pods/SVProgressHUD.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/ActionSheetPicker_3_0.framework"
+  install_framework "Pods/SVProgressHUD.framework"
 fi
