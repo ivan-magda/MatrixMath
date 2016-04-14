6d28b8404b3a8a40052fb524ff17d5b3a335c652
Ivan Magda <imagda15@gmail.com>
2016-04-14 16:45:13 +0900

    feat: MatrixMathApiClient+Convenience
    Add convenience methods to perform math operations on matrix object

---
+++ iOS/MatrixMath/MatrixMath/MatrixMathApiClient+Convenience.swift
@@ -0,0 +1,62 @@
+//
+//  MatrixMathApiClient+Convenience.swift
+//  MatrixMath
+//
+//  Created by Ivan Magda on 14.04.16.
+//  Copyright © 2016 Ivan Magda. All rights reserved.
+//
+
+import Foundation
+
+//-----------------------------------------
+// MARK: - MatrixMathApiClient+Convenience
+//-----------------------------------------
+
+extension MatrixMathApiClient {
+    
+    func performMatrixOperationWithType(type: MatrixOperationType, matrices: Matrix..., withCompletionHandler completionHandler: MatrixMathMatrixResultBlock) {
+        func sendError(error: String) {
+            let userInfo = [NSLocalizedDescriptionKey: error]
+            let error = NSError(domain: MatrixMathApiClient.ErrorDomain,
+                                code: MatrixMathApiClient.ErrorCode,
+                                userInfo: userInfo)
+            completionHandler(matrix: nil, error: error)
+        }
+        
+        guard matrices.count <= 2 else {
+            sendError("Max size of the passed in matrices is equal to 2")
+            return
+        }
+        
+        switch type {
+        case .Addition:
+            addition(left: matrices[0], right: matrices[1], completionHandler: completionHandler)
+        case .Subtract:
+            subtract(left: matrices[0], right: matrices[1], completionHandler: completionHandler)
+        case .Multiply:
+            multiply(left: matrices[0], right: matrices[1], completionHandler: completionHandler)
+        case .Transpose:
+            transpose(matrix: matrices[0], completionHandler: completionHandler)
+        case .Invert:
+            invert(matrix: matrices[0], completionHandler: completionHandler)
+        default:
+            sendError("Incorrect matrix operation type")
+        }
+    }
+    
+    func performSolveOperationWithType(type: MatrixOperationType, coefficientsMatrix matrix: Matrix, valuesVector vector: Array<Double>, completionHandler: MatrixMathSystemSolutionResultBlock) {
+        switch type {
+        case .Solve:
+            solve(coefficientsMatrix: matrix, valuesVector: vector, completionHandler: completionHandler)
+        case .SolveWithErrorCorrection:
+            solveWithErrorCorrection(coefficientsMatrix: matrix, valuesVector: vector, completionHandler: completionHandler)
+        default:
+            let userInfo = [NSLocalizedDescriptionKey: "Incorrect matrix operation type"]
+            let error = NSError(domain: MatrixMathApiClient.ErrorDomain,
+                                code: MatrixMathApiClient.ErrorCode,
+                                userInfo: userInfo)
+            completionHandler(vector: nil, error: error)
+        }
+    }
+    
+}
