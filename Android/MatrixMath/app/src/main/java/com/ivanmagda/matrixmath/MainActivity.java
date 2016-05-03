ecf036c806eab46fc136952e35268cbbc8eab9e0
Ivan Magda <imagda15@gmail.com>
2016-05-03 14:21:20 +0900

    Initial commit

---
+++ Android/MatrixMath/app/src/main/java/com/ivanmagda/matrixmath/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ivanmagda.matrixmath;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
